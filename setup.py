9c5d632d41b7d40039b87e0b81d220c9b9b980f1
Daniel Walker <danielpdwalker@gmail.com>
2021-06-09 18:09:13 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="matatika-datasets-solarvista",
+    version="0.1.0",
+    description="Meltano project file bundle of Matatika datasets for Solarvista",
+    packages=find_packages(),
+    package_data={"bundle": ["analyze/datasets/*.yaml"]},
+)
