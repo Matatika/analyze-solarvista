478b3daa510e75305b2b4dac289ebeb9ec82b547
danielpdwalker <danielpdwalker@gmail.com>
2022-02-08 16:10:55 +0000

    Bumped version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="matatika-datasets-solarvista",
-    version="0.1.0",
+    version="0.1.1",
     description="Meltano project file bundle of Matatika datasets for Solarvista",
     packages=find_packages(),
     package_data={"bundle": ["analyze/datasets/tap-solarvista/*.yaml", "orchestrate/tap-solarvista/elt.sh"]},
