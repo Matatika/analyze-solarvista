55407c053202615d11e4147346b7a0400f291be0
danielpdwalker <danielpdwalker@gmail.com>
2022-02-08 13:37:08 +0000

    Removed env vars from elt script. Removed testing step from elt script. Removed job-id from meltano elt

--- bundle/orchestrate/tap-solarvista/elt.sh
+++ bundle/orchestrate/tap-solarvista/elt.sh
@@ -9,19 +9,6 @@ meltano install loader "$LOADER"
 meltano install transform "$EXTRACTOR"
 meltano install transformer dbt
 
-# Store meltano config and tap state in database
-export MELTANO_DATABASE_URI=postgresql://${TARGET_POSTGRES_USER}:${TARGET_POSTGRES_PASSWORD}@${TARGET_POSTGRES_HOST}:${TARGET_POSTGRES_PORT}/${TARGET_POSTGRES_DBNAME}?options=-csearch_path%3Dpublic
-
-# Transform into our custom target schema (these variables all required by dbt see profiles.yml)
-export DBT_TARGET=postgres
-export PG_ADDRESS=${TARGET_POSTGRES_HOST}
-export PG_PORT=${TARGET_POSTGRES_PORT}
-export PG_USERNAME=${TARGET_POSTGRES_USER}
-export PG_PASSWORD=${TARGET_POSTGRES_PASSWORD}
-export PG_DATABASE=${TARGET_POSTGRES_DBNAME}
-export DBT_SOURCE_SCHEMA=${TARGET_POSTGRES_SCHEMA}
-export DBT_TARGET_SCHEMA=${TARGET_POSTGRES_SCHEMA}
-
 # Install dbt dependencies files
 meltano invoke dbt deps
 
@@ -29,7 +16,6 @@ meltano invoke dbt deps
 meltano invoke dbt run-operation drop_users_stream_table || true
 
 # Run the elt, and dbt commands and tests
-meltano elt tap-solarvista target-postgres --transform=skip --job_id=$TARGET_POSTGRES_SCHEMA
+meltano elt tap-solarvista target-postgres --transform=skip
 meltano invoke dbt snapshot --select tap_solarvista
 meltano invoke dbt run -m tap_solarvista --full-refresh
-meltano invoke dbt test -m tap_solarvista
