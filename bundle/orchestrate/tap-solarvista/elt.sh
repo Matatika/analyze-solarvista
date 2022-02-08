5d425332aad35a1e09b2b5c46567e424bdf59e60
danielpdwalker <danielpdwalker@gmail.com>
2022-02-08 16:01:31 +0000

    Updated elt.sh to only try to store state with a postgres db

--- bundle/orchestrate/tap-solarvista/elt.sh
+++ bundle/orchestrate/tap-solarvista/elt.sh
@@ -9,13 +9,21 @@ meltano install loader "$LOADER"
 meltano install transform "$EXTRACTOR"
 meltano install transformer dbt
 
+# Store meltano config and tap state in database
+if [ $DBT_TARGET = "postgres" ]; then
+    export MELTANO_DATABASE_URI=postgresql://${TARGET_POSTGRES_USER}:${TARGET_POSTGRES_PASSWORD}@${TARGET_POSTGRES_HOST}:${TARGET_POSTGRES_PORT}/${TARGET_POSTGRES_DBNAME}?options=-csearch_path%3Dpublic
+    export MELTANO_JOB_ID="${EXTRACTOR}"_"${LOADER}"_"${TARGET_POSTGRES_SCHEMA}"
+else
+    export MELTANO_JOB_ID="$EXTRACTOR"
+fi
+
 # Install dbt dependencies files
 meltano invoke dbt deps
 
 # Try to drop the users_stream table to allow dbt snapshot to capture hard deleted users from source
-meltano invoke dbt run-operation drop_users_stream_table || true
+meltano invoke dbt run-operation solarvista_drop_users_stream_table || true
 
 # Run the elt, and dbt commands and tests
-meltano elt tap-solarvista target-postgres --transform=skip
+meltano elt "$EXTRACTOR" "$EXTRACTOR" --transform=skip --job_id=$MELTANO_JOB_ID
 meltano invoke dbt snapshot --select tap_solarvista
 meltano invoke dbt run -m tap_solarvista --full-refresh
