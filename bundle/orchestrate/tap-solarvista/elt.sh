89f23d97e2872bb8de79ef47c4f9cb403c2f34e4
danielpdwalker <danielpdwalker@gmail.com>
2022-04-12 16:18:31 +0100

    Updated elt.sh to account for new MELTANO_DATABASE_URI, and added unique job_id

--- bundle/orchestrate/tap-solarvista/elt.sh
+++ bundle/orchestrate/tap-solarvista/elt.sh
@@ -12,14 +12,6 @@ meltano install transformer dbt
 # Temporary fix for markdown dependencies issue: https://github.com/dbt-labs/dbt-core/issues/4745
 .meltano/transformers/dbt/venv/bin/pip3 install --force-reinstall MarkupSafe==2.0.1
 
-# Store meltano config and tap state in database
-if [ $DBT_TARGET = "postgres" ]; then
-    export MELTANO_DATABASE_URI=postgresql://${TARGET_POSTGRES_USER}:${TARGET_POSTGRES_PASSWORD}@${TARGET_POSTGRES_HOST}:${TARGET_POSTGRES_PORT}/${TARGET_POSTGRES_DBNAME}?options=-csearch_path%3Dpublic
-    export MELTANO_JOB_ID="${EXTRACTOR}"_"${LOADER}"_"${TARGET_POSTGRES_SCHEMA}"
-else
-    export MELTANO_JOB_ID="$EXTRACTOR"
-fi
-
 # Install dbt dependencies files
 meltano invoke dbt deps
 
@@ -27,6 +19,6 @@ meltano invoke dbt deps
 meltano invoke dbt run-operation solarvista_drop_users_stream_table || true
 
 # Run the elt, and dbt commands and tests
-meltano elt "$EXTRACTOR" "$LOADER" --transform=skip --job_id=$MELTANO_JOB_ID
+meltano elt "$EXTRACTOR" "$LOADER" --transform=skip --job_id="$EXTRACTOR"-"$LOADER"-"$IMPORTRUNNERID"
 meltano invoke dbt snapshot --select tap_solarvista
 meltano invoke dbt run -m tap_solarvista --full-refresh
