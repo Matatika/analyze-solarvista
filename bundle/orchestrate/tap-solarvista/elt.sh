b111f7f464dc5a120dc7dee62bbda0859bb55708
Reuben Frankel <rfrankel@matatika.com>
2022-06-06 16:42:02 +0100

    Remove `MarkupSafe` reinstall temporary fix

--- bundle/orchestrate/tap-solarvista/elt.sh
+++ bundle/orchestrate/tap-solarvista/elt.sh
@@ -9,9 +9,6 @@ meltano install loader "$LOADER"
 meltano install transform "$EXTRACTOR"
 meltano install transformer dbt
 
-# Temporary fix for markdown dependencies issue: https://github.com/dbt-labs/dbt-core/issues/4745
-.meltano/transformers/dbt/venv/bin/pip3 install --force-reinstall MarkupSafe==2.0.1
-
 # Install dbt dependencies files
 meltano invoke dbt deps
 
